6032941c1400878b0b6e42f7fc432390720eec55
marinechaput <marine.chaput@nerf.be>
2021-04-09 14:23:08 +0200

    add graph parser tests

--- tests/graph_parser_test.cpp
+++ tests/graph_parser_test.cpp
@@ -36,11 +36,11 @@ TEST(expandProcessorName, PartNameWithUnderscore) {
   std::vector<std::string> result = expandProcessorName("processor_name");
   EXPECT_EQ(result[0], "processor-name");
   EXPECT_EQ(result.size(), 1);
-
- // std::vector<std::string> result2 = expandProcessorName("processor_name (1-2)");
- // EXPECT_EQ(result[0], "processor-name1");
- // EXPECT_EQ(result[1], "processor-name2");
- // EXPECT_EQ(result.size(), 2);
+   
+  std::vector<std::string> result2 = expandProcessorName("processor_name (1-2)");
+  EXPECT_EQ(result2[0], "processor-name1");
+  EXPECT_EQ(result2[1], "processor-name2");
+  EXPECT_EQ(result2.size(), 2);
 }
 
 TEST(expandProcessorName, PartNameWithSpace) {
@@ -49,10 +49,10 @@ TEST(expandProcessorName, PartNameWithSpace) {
   EXPECT_EQ(result[0], "processor-name");
   EXPECT_EQ(result.size(), 1);
 
-/*  std::vector<std::string> result2 = expandProcessorName("processor name (1-2)");
-  EXPECT_EQ(result[0], "processor-name1");
-  EXPECT_EQ(result[1], "processor-name2");
-  EXPECT_EQ(result.size(), 2);*/
+  std::vector<std::string> result2 = expandProcessorName("processor name (1-2)");
+  EXPECT_EQ(result2[0], "processor-name1");
+  EXPECT_EQ(result2[1], "processor-name2");
+  EXPECT_EQ(result2.size(), 2);
 }
 
 TEST(expandProcessorName, PartNameWithDash) {
@@ -61,23 +61,77 @@ TEST(expandProcessorName, PartNameWithDash) {
   EXPECT_EQ(result[0], "processor-name");
   EXPECT_EQ(result.size(), 1);
 
- /* std::vector<std::string> result2 = expandProcessorName("processor-name (1-2)");
-  EXPECT_EQ(result[0], "processor-name1");
-  EXPECT_EQ(result[1], "processor-name2");
-  EXPECT_EQ(result.size(), 2);*/
+  std::vector<std::string> result2 = expandProcessorName("processor-name (1-2)");
+  EXPECT_EQ(result2[0], "processor-name1");
+  EXPECT_EQ(result2[1], "processor-name2");
+  EXPECT_EQ(result2.size(), 2);
 }
 
 TEST(ParseConnectionRules, classicRule) {
-  ConnectionRule rules = parseConnectionRule("source.hp=ripple_filter.data");
-  //EXPECT_EQ(printSingleConnectionRule(rules.first), "0source.1hp.2");
-  //EXPECT_EQ(printSingleConnectionRule(rules.second), "0ripple-filter.1data.2");
+  ConnectionRule rules = parseConnectionRule("source.hp.0=ripple_filter.data.1");
+
+  // PROCESSOR PART 
+  printConnectionRule(rules);
+  EXPECT_EQ(std::get<1>(rules.second[0]), "ripple-filter");
+  EXPECT_EQ(std::get<1>(rules.first[0]), "source");
+  EXPECT_EQ(std::get<0>(rules.second[0]), 0);
+  EXPECT_EQ(std::get<0>(rules.first[0]), 0);
+
+  // PORT PART 
+  
+  EXPECT_EQ(std::get<1>(rules.second[1]), "data");
+  EXPECT_EQ(std::get<1>(rules.first[1]), "hp");
+  EXPECT_EQ(std::get<0>(rules.second[1]), 1);
+  EXPECT_EQ(std::get<0>(rules.first[1]), 1);
+
+  // SLOT PART 
+  
+  EXPECT_EQ(std::get<2>(rules.second[2])[0], 1);
+  EXPECT_EQ(std::get<2>(rules.first[2])[0], 0);
+  EXPECT_EQ(std::get<0>(rules.second[2]), 2);
+  EXPECT_EQ(std::get<0>(rules.first[2]), 2);
+
 }
 
 TEST(ParseConnectionRules, ExpandRule) {
-  ConnectionRule rules = parseConnectionRule("ripple_filter.data = p:data.f:detector_sink");
-  //EXPECT_EQ(printSingleConnectionRule(rules.first), "0ripple_filter.1data.2");
-  //EXPECT_EQ(printSingleConnectionRule(rules.second), "0detector_sink.1data.2");
+  ConnectionRule rules = parseConnectionRule("source.hp = f:ripple_filter.p:data.s:1");
+
+  printConnectionRule(rules);
+  EXPECT_EQ(std::get<1>(rules.second[0]), "ripple-filter");
+  EXPECT_EQ(std::get<1>(rules.first[0]), "source");
+  EXPECT_EQ(std::get<0>(rules.second[0]), 0);
+  EXPECT_EQ(std::get<0>(rules.first[0]), 0);
+
+  // PORT PART 
+  EXPECT_EQ(std::get<1>(rules.second[1]), "data");
+  EXPECT_EQ(std::get<1>(rules.first[1]), "hp");
+  EXPECT_EQ(std::get<0>(rules.second[1]), 1);
+  EXPECT_EQ(std::get<0>(rules.first[1]), 1);
+
+  // SLOT PART 
+  EXPECT_EQ(std::get<2>(rules.second[2])[0], 1);
+  EXPECT_EQ(std::get<2>(rules.first[2])[0], -1);
+  EXPECT_EQ(std::get<0>(rules.second[2]), 2);
+  EXPECT_EQ(std::get<0>(rules.first[2]), 2);
 }
+
+TEST(ParseConnectionRules, DocEquivalentyRules) {
+  ConnectionRule rules = parseConnectionRule("upstream.out(1-2)=downstream.in(1-2)");
+
+  EXPECT_EQ(std::get<1>(rules.first[0]), "upstream");
+  EXPECT_EQ(std::get<1>(rules.second[0]), "downstream");
+  EXPECT_EQ(std::get<0>(rules.second[0]), 0);
+  EXPECT_EQ(std::get<0>(rules.first[0]), 0);
+
+  // PORT PART 
+  EXPECT_EQ(std::get<1>(rules.first[1]), "out");
+  EXPECT_EQ(std::get<1>(rules.second[1]), "in");
+  EXPECT_EQ(std::get<0>(rules.second[1]), 1);
+  EXPECT_EQ(std::get<0>(rules.first[1]), 1);
+  EXPECT_EQ(std::get<2>(rules.first[1])[0], 1);
+  EXPECT_EQ(std::get<2>(rules.first[1])[1], 2);
+}
+
 
 
 } // namespace
