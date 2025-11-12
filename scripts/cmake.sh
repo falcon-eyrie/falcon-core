d0a2875b7d9473770589a0d6a8d7e5a8e62ae282
ben <benfrost42@icloud.com>
2025-11-12 14:37:07 +0100

    Fix compilation errors

---
+++ scripts/cmake.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+if ! command -v cmake >/dev/null 2>&1; then
+    echo "Error: CMake is not installed."
+    echo "You can install it using: scripts/setup/install_cmake.sh"
+    exit 1
+fi
+
+mkdir -p build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_POLICY_VERSION_MINIMUM=3.5
