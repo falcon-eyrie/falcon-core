dfbf7724a2d0b102de698f944078fa92c12150c9
ben <benfrost42@icloud.com>
2025-12-06 16:42:13 +0100

    clang-tidy

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -1,4 +1,4 @@
-run-clang-tidy -p build $(find . \
+run-clang-tidy -p build -j $(nproc 2>/dev/null || sysctl -n hw.logicalcpu)$(find . \
   -name '*.c'    -o -name '*.cc'   -o -name '*.cpp'  -o -name '*.cxx' -o -name '*.c++' \
   -o -name '*.h' -o -name '*.hh'   -o -name '*.hpp'  -o -name '*.hxx' -o -name '*.h++' \
-  | grep -v "./build/")
+  | grep -v "./build/" -v "./extensions/")
