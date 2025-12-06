1f87f83b4080874e5d4ce3aa849cc9cd423f1003
ben <benfrost42@icloud.com>
2025-12-06 17:55:54 +0100

    wip

--- scripts/setup/install_llvm.sh
+++ scripts/setup/install_llvm.sh
@@ -16,11 +16,15 @@ tar -xf "$ARCHIVE" -C llvm-extract --strip-components=1
 
 echo "Extracted LLVM archive into llvm-extract/"
 
-echo "Moving to /opt/llvm-${VERSION} ..."
-sudo mv llvm-extract "/opt/llvm-${VERSION}"
+echo "Installing to /usr/local/llvm-${VERSION} ..."
+sudo rm -rf "/usr/local/llvm-${VERSION}"
+sudo mv llvm-extract "/usr/local/llvm-${VERSION}"
+
+echo "Creating symlinks in /usr/local/bin ..."
+sudo ln -sf /usr/local/llvm-${VERSION}/bin/* /usr/local/bin/
 
 echo "Cleaning up $ARCHIVE..."
 rm -f "$ARCHIVE"
 
 echo "LLVM-${VERSION} installed successfully."
-echo "----------------------------------"
+clang-format --version
