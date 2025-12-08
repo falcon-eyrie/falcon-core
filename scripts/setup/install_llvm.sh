af604e68ade50bcd4a6487405ed09cc092af6fc1
ben <benfrost42@icloud.com>
2025-12-08 16:36:17 +0100

    fix installation script

--- scripts/setup/install_llvm.sh
+++ scripts/setup/install_llvm.sh
@@ -3,28 +3,27 @@ set -e
 
 VERSION=21.1.7
 
-cd /tmp
+cd /usr/local
 
 ARCHIVE="LLVM-${VERSION}.tar.xz"
 URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/LLVM-${VERSION}-Linux-X64.tar.xz"
 
 echo "Downloading LLVM $VERSION..."
-curl -fsSL -o "$ARCHIVE" "$URL"
+sudo curl -fsSL -o "$ARCHIVE" "$URL"
 
-mkdir -p llvm-extract
-tar -xf "$ARCHIVE" -C llvm-extract --strip-components=1
+echo "Extracting LLVM archive into llvm-${VERSION}/..."
 
-echo "Extracted LLVM archive into llvm-extract/"
-
-echo "Installing to /usr/local/llvm-${VERSION} ..."
 sudo rm -rf "/usr/local/llvm-${VERSION}"
-sudo mv llvm-extract "/usr/local/llvm-${VERSION}"
+sudo mkdir -p "llvm-${VERSION}"
+sudo tar -xf "$ARCHIVE" -C "llvm-${VERSION}" --strip-components=1
+
+echo "Extracted LLVM archive into llvm-${VERSION}/"
 
 echo "Creating symlinks in /usr/local/bin ..."
 sudo ln -sf /usr/local/llvm-${VERSION}/bin/* /usr/local/bin/
 
 echo "Cleaning up $ARCHIVE..."
-rm -f "$ARCHIVE"
+sudo rm -f "$ARCHIVE"
 
 echo "LLVM-${VERSION} installed successfully."
 clang --version
