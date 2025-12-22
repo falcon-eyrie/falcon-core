d0f78531cf9a782b65be36faac3563cdbd2535eb
ben <benfrost42@icloud.com>
2025-12-22 15:20:21 +0100

    wip

--- falcon_gui/lib/main.dart
+++ falcon_gui/lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
@@ -6,12 +6,13 @@ import 'package:window_manager/window_manager.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await windowManager.ensureInitialized();
+  await windowManager.setBackgroundColor(Colors.transparent);
 
   const windowOptions = WindowOptions(
-    titleBarStyle: TitleBarStyle.hidden, // hides toolbar/titlebar
+    titleBarStyle: TitleBarStyle.hidden,
     size: Size(800, 600),
     center: true,
-    skipTaskbar: true,
+    skipTaskbar: false,
     title: 'Falcon GUI',
     windowButtonVisibility: false,
   );
@@ -29,12 +30,9 @@ class DesktopApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Desktop Complex App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
@@ -46,201 +44,99 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  int _counter = 0;
-  late AnimationController _controller;
-  late Animation<double> _animation;
-  bool _showSecondPage = false;
-  final List<String> _items = List.generate(10, (i) => 'Item ${i + 1}');
-  final Random _random = Random();
-  Color _bgColor = Colors.white;
+class _HomePageState extends State<HomePage> with WindowListener {
+  bool isMaximized = false;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    // ignore: discarded_futures
-    )..repeat(reverse: true);
-    _animation = Tween<double>(begin: 0, end: 200).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-    );
+    windowManager.addListener(this);
+    unawaited(_syncState());
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-      _bgColor = Color.fromARGB(
-        255,
-        _random.nextInt(256),
-        _random.nextInt(256),
-        _random.nextInt(256),
-      );
-    });
+  Future<void> _syncState() async {
+    isMaximized = await windowManager.isMaximized();
+    if (mounted) setState(() {});
   }
 
-  void _togglePage() {
-    setState(() {
-      _showSecondPage = !_showSecondPage;
-    });
+  @override
+  void onWindowMaximize() {
+    setState(() => isMaximized = true);
+  }
+
+  @override
+  void onWindowUnmaximize() {
+    setState(() => isMaximized = false);
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    windowManager.removeListener(this);
     super.dispose();
-  }
-
-  Widget _buildMenu() {
-    return Drawer(
-      child: ListView(
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(color: Colors.blue),
-            child: Text(
-              'Menu',
-              style: TextStyle(color: Colors.white, fontSize: 24),
-            ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home'),
-            onTap: () {
-              setState(() {
-                _showSecondPage = false;
-              });
-              Navigator.of(context).pop();
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.pages),
-            title: const Text('Second Page'),
-            onTap: () {
-              setState(() {
-                _showSecondPage = true;
-              });
-              Navigator.of(context).pop();
-            },
-          ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.exit_to_app),
-            title: const Text('Exit'),
-            onTap: () async {
-              await Navigator.of(context).maybePop();
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildFirstPage() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text('Counter: $_counter', style: const TextStyle(fontSize: 32)),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: _incrementCounter,
-          child: const Text('Increment & Random Color'),
-        ),
-        const SizedBox(height: 20),
-        AnimatedBuilder(
-          animation: _animation,
-          builder: (context, child) {
-            return Container(
-              width: _animation.value,
-              height: _animation.value,
-              decoration: BoxDecoration(
-                color: Colors.redAccent,
-                borderRadius: BorderRadius.circular(_animation.value / 2),
-              ),
-            );
-          },
-        ),
-        const SizedBox(height: 20),
-        SizedBox(
-          height: 150,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: _items.length,
-            itemBuilder: (context, index) {
-              return Container(
-                width: 120,
-                margin: const EdgeInsets.symmetric(horizontal: 8),
-                color: Colors.blue[_random.nextInt(9) * 100 + 100],
-                child: Center(
-                  child: Text(
-                    _items[index],
-                    style: const TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                ),
-              );
-            },
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSecondPage() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Text('Second Page', style: TextStyle(fontSize: 32)),
-        const SizedBox(height: 20),
-        Wrap(
-          spacing: 10,
-          runSpacing: 10,
-          children: List.generate(20, (index) {
-            return GestureDetector(
-              onTap: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Clicked box ${index + 1}')),
-                );
-              },
-              child: Container(
-                width: 60,
-                height: 60,
-                color: Colors.primaries[index % Colors.primaries.length],
-              ),
-            );
-          }),
-        ),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: _togglePage,
-          child: const Text('Back to Home'),
-        ),
-      ],
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: _bgColor,
-      appBar: AppBar(
-        title: const Text('Flutter Desktop Complex App'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.refresh),
-            onPressed: () {
-              setState(() {
-                _bgColor = Colors.white;
-                _counter = 0;
-              });
+      body: Column(
+        children: [
+          GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onPanStart: (_) {
+              unawaited(windowManager.startDragging());
             },
+            child: Container(
+              height: 40,
+              width: double.infinity,
+              color: Colors.grey.shade900,
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Row(
+                children: [
+                  const Text(
+                    'Falcon GUI',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.remove,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    onPressed: windowManager.minimize,
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      isMaximized ? Icons.filter_none : Icons.crop_square,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    onPressed: () async {
+                      if (isMaximized) {
+                        await windowManager.unmaximize();
+                      } else {
+                        await windowManager.maximize();
+                      }
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    onPressed: windowManager.close,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Expanded(
+            child: Center(
+              child: Text('Hello'),
+            ),
           ),
         ],
-      ),
-      drawer: _buildMenu(),
-      body: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 500),
-        child: _showSecondPage ? _buildSecondPage() : _buildFirstPage(),
       ),
     );
   }
